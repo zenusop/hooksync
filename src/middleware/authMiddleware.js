d0330ae06a3cb6d4897b841d7cc151d97d01145b
Zenus <75637878+zenusop@users.noreply.github.com>
2025-02-22 18:55:10 -0800

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,1 +1,9 @@
+// Middleware to check if user is authenticated
+module.exports = function(req, res, next) {
+  if (req.session && req.session.user) {
+    return next();
+  } else {
+    res.redirect('/auth/login');
+  }
+};
 
