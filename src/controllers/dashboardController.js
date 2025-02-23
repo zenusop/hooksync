d9f2c60b5bf7a32222aa7d700f739020965f5197
Zenus <75637878+zenusop@users.noreply.github.com>
2025-02-22 18:54:55 -0800

    Update dashboardController.js

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,1 +1,7 @@
+// Controller for dashboard logic
+
+exports.showDashboard = (req, res) => {
+  // Here you might fetch data to display on the dashboard.
+  res.render('dashboard', { user: req.session.user });
+};
 
