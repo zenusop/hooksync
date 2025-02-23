57e426f50c73596a638aa7082f9f321d601a1660
Zenus <75637878+zenusop@users.noreply.github.com>
2025-02-22 18:01:26 -0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,5 +6,5 @@
 define('ADMIN_USERNAME', 'admin');
 
 // This is a bcrypt hash for the password "secret"
-define('ADMIN_HASH', '$2y$10$abcdefg...'); 
+define('ADMIN_HASH', '$2y$10$W0v6x7r5YG1zV31ZihLNaOh2qC6flW5IVDI5X26Eux9q1D3Z2//Ei'); 
 // Generate your own hash with password_hash('secret', PASSWORD_DEFAULT);
