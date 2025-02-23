04f50305b7c9d69cf74d49d8a967e8a187719a81
Zenus <75637878+zenusop@users.noreply.github.com>
2025-02-22 17:54:22 -0800

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,1 +1,48 @@
+<?php
+// functions.php
 
+/**
+ * Load an array from a JSON file.
+ * If file doesn't exist or is empty, return an empty array.
+ */
+function loadJson($path) {
+    if (!file_exists($path)) {
+        return [];
+    }
+    $json = file_get_contents($path);
+    $data = json_decode($json, true);
+    return is_array($data) ? $data : [];
+}
+
+/**
+ * Save an array to a JSON file, overwriting existing content.
+ */
+function saveJson($path, $array) {
+    file_put_contents($path, json_encode($array, JSON_PRETTY_PRINT));
+}
+
+/**
+ * Get all webhooks from data/webhooks.json
+ */
+function getAllWebhooks() {
+    return loadJson(__DIR__ . '/../data/webhooks.json');
+}
+
+/**
+ * Save an array of webhooks to data/webhooks.json
+ */
+function saveAllWebhooks($webhooks) {
+    saveJson(__DIR__ . '/../data/webhooks.json', $webhooks);
+}
+
+/**
+ * Find a webhook by ID in the array
+ */
+function findWebhook($id, $webhooks) {
+    foreach ($webhooks as $wh) {
+        if ($wh['id'] == $id) {
+            return $wh;
+        }
+    }
+    return null;
+}
